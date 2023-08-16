ebfb3be80148edefde89fe09376c80457e787865
quavodanceq <kuat.oralbekov1011@mail.ru>
2023-08-17 01:40:10 +0600

    Initial Commit

---
+++ ScaleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScaleApp
+//
+//  Created by Куат Оралбеков on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
